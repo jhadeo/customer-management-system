a6b11e322a42483a84b41631c1adbe766056526b
jhadeo <105715821+jhadeo@users.noreply.github.com>
2026-07-23 18:31:22 +0800

    feat: made CustomerController, StoreCustomerRequest, UpdateCustomerRequest boilerplate files
    TODO: make CRUD operations

---
+++ api/app/Http/Requests/UpdateCustomerRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name'  => ['required', 'string', 'max:255'],
+            'email'      => ['required', 'email', Rule::unique('customers', 'email')->ignore($this->route(('id')))],
+        ];
+    }
+}
